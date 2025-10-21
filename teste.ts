426462687dbb376f257a0f3504dda2e9699d988a
LorenzoLopesSilva <lorenzo.lopes0808@gmail.com>
2025-10-21 16:47:50 -0300

    first commit

---
+++ teste.ts
@@ -0,0 +1,17 @@
+//Boleano
+let isActive: boolean = true;
+let hasPermission = false;
+
+//Numero
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+let floatNumber: number = 3.14;
+
+//String
+let color: string = "blue";
+let fullName: string = 'Lorenzo Lopes';
+let age: number = 19;
+let sentence: string = `Olá, meu nome é ${fullName} e eu irei fazer ${age + 1}`;
+
