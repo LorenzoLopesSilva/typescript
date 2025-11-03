6a8352f4bdc9360e2b09ae71e2755853ca96cb52
Lorenzo Lopes <lorenzo.lopes0808@gmail.com>
2025-11-03 10:35:21 -0300

    Parâmetros Definidos

---
+++ parametros-opcionais-padrao/parametros-defindidos.js
@@ -0,0 +1,6 @@
+function saudar(nome) {
+    if (nome === void 0) { nome = "Visitante"; }
+    console.log("Ol\u00E1 ".concat(nome, "!"));
+}
+saudar();
+saudar("Lorenzo");
---
+++ parametros-opcionais-padrao/parametros-defindidos.ts
@@ -0,0 +1,6 @@
+function saudar(nome: string = "Visitante"){
+    console.log(`Olá ${nome}!`)
+}
+
+saudar()
+saudar("Lorenzo")
