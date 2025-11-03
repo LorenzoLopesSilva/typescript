a2cea79ebe01da272f4115ee81f0a2c56db8dd43
Lorenzo Lopes <lorenzo.lopes0808@gmail.com>
2025-11-03 11:31:24 -0300

    Funções anônimas e arrow functions

--- funcoes/anonimas/funcoes-anonimas.js
+++ funcoes/anonimas/funcoes-anonimas.js
@@ -8,3 +8,16 @@ var numeros = [10, 20, 30];
 numeros.forEach(function (n) {
     console.log(n);
 });
+var pessoas = [
+    { nome: "Lorenzo", idade: 19 },
+    { nome: "Ingrid", idade: 17 },
+    { nome: "Rafa", idade: 19 }
+];
+// let maiores = pessoas.filter((n: Pessoa) => n.idade >= 18);
+var maiores = pessoas.filter(function (n) {
+    return n.idade >= 18; //NÂO ESQUECER O RETURN
+});
+for (var _i = 0, maiores_1 = maiores; _i < maiores_1.length; _i++) {
+    var pessoa = maiores_1[_i];
+    console.log("".concat(pessoa.nome, " \u00E9 maior de idade"));
+}
--- funcoes/anonimas/funcoes-anonimas.ts
+++ funcoes/anonimas/funcoes-anonimas.ts
@@ -13,3 +13,25 @@ const numeros: number[] = [10, 20, 30];
 numeros.forEach(function(n){
     console.log(n)
 })
+
+//------------------ EXEMPLO ------------------
+
+interface Pessoa{
+    nome: string;
+    idade: number;
+}
+
+let pessoas: Pessoa[] = [
+    {nome: "Lorenzo", idade: 19},
+    {nome: "Ingrid", idade: 17},
+    {nome: "Rafa", idade: 19}
+]
+
+// let maiores = pessoas.filter((n: Pessoa) => n.idade >= 18);
+let maiores: Pessoa[] = pessoas.filter(function(n: Pessoa){
+    return n.idade >= 18; //NÂO ESQUECER O RETURN
+});
+
+for(let pessoa of maiores){
+    console.log(`${pessoa.nome} é maior de idade`);
+}
